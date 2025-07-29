d2b32f1ce5b70b1d63ca7e2021495694654db706
Pavan Chinnadorai <pavanchinnadoraij1@gmail.com>
2025-07-29 11:33:09 +0530

    updated axiosInstance

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'https://mlanadtuskers.onrender.com/api', 
+  withCredentials: true
 });
 axiosInstance.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
